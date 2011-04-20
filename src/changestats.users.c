4125b8fb12caf061f31c8c13bff4621ba265e6ce
David R. Hunter <dhunter@stat.psu.edu>
2011-04-20 15:37:22 +0000

    rewrote some of the code to reflect (tails, heads) switches that were missed and explain this better.  Also added IS_UNDIRECTED_EDGE macro in places.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergmuserterms/trunk@11135 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/changestats.users.c
+++ src/changestats.users.c
@@ -48,3 +48,52 @@ CHANGESTAT_FN(d_mindegree) {
 	}
 	UNDO_PREVIOUS_TOGGLES(i); /* Needed on exit in case of multiple toggles */
 }
+
+CHANGESTAT_FN(d_mindegree) {
+  Vertex t, h, node3;
+  int i, mindeg, hdeg, tdeg;
+  Edge e;
+  int attrflag;
+  double t_nodecov, h_nodecov;
+
+  ZERO_ALL_CHANGESTATS(i);
+  FOR_EACH_TOGGLE(i) {
+    t = TAIL(i); h = HEAD(i);
+    attrflag = INPUT_PARAM[0];
+    mindeg = INPUT_PARAM[1];
+    if(attrflag==0){
+      tdeg = IN_DEG[t]+OUT_DEG[t];
+      hdeg = IN_DEG[h]+OUT_DEG[h];
+      CHANGE_STAT[0] += IS_UNDIRECTED_EDGE(t,h) ?
+        - (tdeg==mindeg) - (hdeg==mindeg) :
+        (tdeg==mindeg-1) + (hdeg==mindeg-1);
+    }else{
+      t_nodecov = INPUT_PARAM[t+1];
+      h_nodecov = INPUT_PARAM[h+1];
+      if (t_nodecov == h_nodecov) {
+        tdeg = 0;
+        STEP_THROUGH_OUTEDGES(t, e, node3) { /* step through outedges of tail */
+          if(INPUT_PARAM[node3+1]==t_nodecov){++tdeg;}
+        }
+        STEP_THROUGH_INEDGES(t, e, node3) { /* step through inedges of tail */
+          if(INPUT_PARAM[node3+1]==t_nodecov){++tdeg;}
+        }
+        hdeg = 0;
+        STEP_THROUGH_OUTEDGES(h, e, node3) { /* step through outedges of head */
+          if(INPUT_PARAM[node3+1]==h_nodecov){++hdeg;}
+        }
+        STEP_THROUGH_INEDGES(h, e, node3) { /* step through inedges of head */
+          if(INPUT_PARAM[node3+1]==h_nodecov){++hdeg;}
+        }
+        CHANGE_STAT[0] += IS_UNDIRECTED_EDGE(t,h) ?
+          - (tdeg==mindeg) - (hdeg==mindeg) :
+          (tdeg==mindeg-1) + (hdeg==mindeg-1);
+      }else{
+        CHANGE_STAT[0] = 0;
+      }
+    }
+    TOGGLE_IF_MORE_TO_COME(i);
+  }
+  UNDO_PREVIOUS_TOGGLES(i);
+}
+
