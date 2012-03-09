22cbfb33f6f9a482fb3416a77a3ec710bbe1609d
David R. Hunter <dhunter@stat.psu.edu>
2012-03-09 18:39:00 +0000

    removed JSS paper from package entirely in order to pass R CMD check (ergm.userterms 3.0-1 is now on CRAN).  Putting the paper back into the package will involve some work, since evidently it is no longer allowed to simply dump a bunch of .tex and .pdf files into the inst/doc directory.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergmuserterms/trunk@11495 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- inst/doc/code.R
+++
@@ -1,8 +0,0 @@
-library(ergm.userterms)
-set.seed(123)
-ntwk <- network(10, directed = FALSE)
-lab <- letters[rep(1:2, 5)]
-ntwk %v% "labels" <- lab
-plot(ntwk, label = lab, label.pos = 5, vertex.cex = 2.5) 
-summary(ntwk ~ mindegree(2) + mindegree(2, by = "labels"))
-
