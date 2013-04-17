02a4bd88f266fe53cb8a65b342c01c56a5afea4a
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-04-17 19:23:39 +0000

    Added copyright headers to the 3.1 release branches.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergmuserterms/branches/3.1@11952 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmTerm.users.R
+++ R/InitErgmTerm.users.R
@@ -1,3 +1,12 @@
+#  File R/InitErgmTerm.users.R in package ergm.userterms, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2003-2013 Statnet Commons
+#######################################################################
 ######################################################################
 #
 # !! USERS: READ THIS FIRST!!
