123d7acccc62e23f24d13235f4f49ab2a3d48ec0
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-04-26 21:43:47 +0000

    * ergm.userterms now has a startup message about checking eut-update help for instructions. * Copied eut-upgrade.Rd to ergm.userterms as well. * Removed spurious executable property from a bunch of files.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergmuserterms/trunk@11977 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
 .onAttach <- function(lib, pkg){
   sm <- statnetStartupMessage("ergm.userterms", c("statnet"), FALSE)
-  if(!is.null(sm)) packageStartupMessage(sm)
+  if(!is.null(sm)){
+    packageStartupMessage(sm)
+    packageStartupMessage(paste(c(strwrap(paste("NOTE: If you use custom ERGM terms based on ",sQuote("ergm.userterms")," version prior to 3.1, you will need to perform a one-time update of the package boilerplate files (the files that you did not write or modify) from ",sQuote("ergm.userterms")," 3.1 or later. See help('eut-upgrade') for instructions.",sep="")),""),collapse="\n"))
+  }
 }
 
 
