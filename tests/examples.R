280f9ec26c7ddf291c48d1d5a5cb85d3936a88fa
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-08-25 23:15:20 +1000

    Removed an unnecessary opttest().

--- tests/examples.R
+++ tests/examples.R
@@ -1,11 +1,15 @@
+#  File tests/examples.R in package ergm.userterms, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2012-2018 Statnet Commons
+#######################################################################
 library(ergm.userterms)
-opttest({
-rm(list=ls())
-{
 data(florentine)
 summary(flomarriage~mindegree(3))
 summary(flomarriage~mindegree(1,by="priorates"))
 fit <- ergm(flomarriage~edges+mindegree(1,by="priorates"))
 summary(fit)
-}
-},"mindegree-term.Rd")
