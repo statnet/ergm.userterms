82d6fa47a5ba777bd3d42aa719364f592a9fa615
Pavel N. Krivitsky <pavel@uow.edu.au>
2019-05-14 20:34:14 +1000

    requireNamespaceTest now uses getS3method() as it should.

--- tests/requireNamespaceTest.R
+++ tests/requireNamespaceTest.R
@@ -11,5 +11,5 @@ library(network)
 requireNamespace('ergm')   #load the namespace, but don't attach the package
 # try loading a term from another package namespace
 requireNamespace('ergm.userterms')  # NOTE: this now loads ergm normally because it id a Depends of eut
-ergm::summary.statistics.formula(network.initialize(2,directed=FALSE)~mindegree(2))
-ergm::summary.statistics.formula(network.initialize(2,directed=FALSE)~edges)
+getS3method("summary","formula")(network.initialize(2,directed=FALSE)~mindegree(2))
+getS3method("summary","formula")(network.initialize(2,directed=FALSE)~edges)
